2d8c99c3ba648ffe91134d7195cd333523cea21b
Volodymyr Rykhva <volodymyr.rykhva@sigma.software>
2018-09-20 19:08:32 +0300

    CocoaPods init commit

---
+++ CocoaPodsApp/CocoaPodsApp/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  CocoaPodsApp
+//
+//  Created by Vova on 9/20/18.
+//  Copyright © 2018 Vova. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
---
+++ CocoaPodsApp/CocoaPodsApp/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  CocoaPodsApp
+//
+//  Created by Vova on 9/20/18.
+//  Copyright © 2018 Vova. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
